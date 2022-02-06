55014c0910ea23277e4c72b48db418990c1558b2
na0i <nayoi1oo5@gmail.com>
2022-02-07 00:18:25 +0900

    220206

--- 실습 파일/2. 자바스크립트 기초 강의/1. calculator-exam/Calculator.mjs
+++ 실습 파일/2. 자바스크립트 기초 강의/1. calculator-exam/Calculator.mjs
@@ -5,14 +5,35 @@ function showPrompt(){
   console.clear();
   // const testStr = readLine.question('Enter test message : ')
   const firstNumber = question('Enter first number : ');
-  // const operation = question('Enter operation : ');
-  // const secondNumber = question('Enter second number : ');
+  const operation = question('Enter operation : ');
+  const secondNumber = question('Enter second number : ');
  
-  console.log(isNumber(firstNumber));
+  const validation = isNumber(firstNumber) && isOperator(operation) && isNumber(secondNumber);
+
+  if (validation){
+    let result = calculate(parseInt(firstNumber), operation, parseInt(secondNumber));
+    console.log(`${result}`);
+  } else {
+    console.log('Invalid Input')
+  }
 }
 
 function isNumber(number){
   return ! isNaN(parseInt(number));
+}
+
+function isOperator(operation){
+  let operations = '+-*/'
+  return operations.indexOf(operation) !== -1;
+}
+
+function calculate(firstNumber, operation, secondNumber) {
+  switch(operation) {
+    case '+': return firstNumber + secondNumber;
+    case '-': return firstNumber - secondNumber;
+    case '*': return firstNumber * secondNumber;
+    case '/': return firstNumber / secondNumber;
+  }
 }
 
 showPrompt();
